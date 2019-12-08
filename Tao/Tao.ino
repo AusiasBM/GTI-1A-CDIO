28e283c54f19dd1128084ba521cfd2999bbb7a4d
Ausias <ausiasbm@gmail.com>
2019-12-08 23:43:42 +0100

    Código organizado

--- Tao/Tao.ino
+++ Tao/Tao.ino
@@ -1,5 +1,10 @@
+// ***************** Librerias ******************
 #include <Wire.h>
 #include "SensorObjeto.h"
+#include <TinyGPS++.h>  //Librería del GPS
+#include <SoftwareSerial.h>
+
+// ***************** Constantes Acelerómetro *******************
 #define    MPU9250_ADDRESS            0x68
 #define    MAG_ADDRESS                0x0C
 #define    GYRO_FULL_SCALE_250_DPS    0x00
@@ -10,14 +15,14 @@
 #define    ACC_FULL_SCALE_4_G        0x08
 #define    ACC_FULL_SCALE_8_G        0x10
 #define    ACC_FULL_SCALE_16_G       0x18
-#include <TinyGPS++.h>  //Librería del GPS
-#include <SoftwareSerial.h>
+
+// ***************** Constantes GPS *******************
 #define RX_PIN  12 // GPS RXI
 #define TX_PIN  13 // GPS TX0
 #define INIT_PIN 15 // Pin para  Inicializar el GPS
 #define GPS_BAUD  4800  //  velocidad de comunicación serie 
 
-// Constantes y variables globales
+// *********** Constantes y variables globales ************
 const int OscuridadValue = 0.027;
 const int SombraValue = 1.70;
 const int LuzValue = 2.40;
@@ -25,33 +30,35 @@ const int MuchaLuzValue = 3.75;
 const byte interruptPin = 4;
 const int sleepTimeS = 5;
 
-// Constantes Salinidad Analógicas
+// *********** Constantes Salinidad Analógicas ***********
 const int SinSalValue = 1300;  // Medimos valor sin sal 
 const int SalValue = 19300;  // Medimos valor con sal
 
-// Constantes Humedad Analógicas
+// *********** Constantes Humedad Analógicas ***********
 const int AirValue = 20470;  // Medimos valor aire
 const int WaterValue = 9600;  // Medimos valor con Agua
 
-// ADC que hemos definido nosotros en el Hadware
+// *********** ADC que hemos definido nosotros en el Hadware ***********
 int adcHumedad = 0;
 int adcSalinidad = 1;
 int adcTemperatura = 2;
 
-// Iniciar un objeto Sensor
+// *********** Iniciar un objeto Sensor ***********
 Sensor sensores;
 
-// Definimos el objeto gps
+// *********** Definimos el objeto gps ***********
 TinyGPSPlus gps; 
 
-// Pin de salinidad
+// *********** Pin de salinidad ***********
 int salinity_power_pin = 5;
 
-// Creamos una UART software para comunicación con el GPS
+// *********** Creamos una UART software para comunicación con el GPS ***********
 SoftwareSerial ss(RX_PIN,TX_PIN); 
-       
-        /*FUNCIONES*/
-        
+
+/******************************************************************
+************ Funciones para programar el acelerómetro ************
+******************************************************************/
+
 //Funcion auxiliar lectura
 void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
 {
@@ -74,6 +81,11 @@ void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
   Wire.write(Data);
   Wire.endTransmission();
 }
+
+/*************************************************************************
+************ Funciones para la interrupción (Wake on motion) ************
+*************************************************************************/
+
 //Wake on motion
 void handleInterrupt(){
   uint8_t intStatus[1];
@@ -81,64 +93,10 @@ void handleInterrupt(){
  // Serial.println("interrupción");
 }
 
-void medicionLuminosidad(){
+/******************************************************************
+*********************** Funciones GPS ****************************
+******************************************************************/
 
-  double adc3, lum, luminosidad;
-
-  adc3 = ads1115.readADC_SingleEnded(3);
-  
-  lum=calcularLuminosidad(adc3);
-  luminosidad = 100*OscuridadValue/(OscuridadValue-MuchaLuzValue)-lum*100/(OscuridadValue-MuchaLuzValue);
-  mostrarLuminosidad(luminosidad);
-  //Serial.print("AIN0 Luminosidad: ");
-  //Serial.println(lum);
-}
-
-double calcularLuminosidad(double adcval){
-  double vol;
- 
-  vol = ((adcval*4096)/32767)/1000;
-  return vol;
-}
-
-void  mostrarLuminosidad(double luminosidad){
-
-   Serial.print("Hay una Luminosidad del ");
-
-   if(luminosidad <= 100){
-    
-    Serial.print(luminosidad);
-   
-   }else{
-    
-    luminosidad = 100;
-    Serial.print(luminosidad);
-   
-   }
-   Serial.println(" %");
-
-  if(luminosidad < 1.50){
-
-    Serial.println("Por tanto o es de noche o hay mucha oscuridad.");
-  }
-  
-  if(luminosidad > 1.50 && luminosidad < 50){
-
-    Serial.println("Por tanto es muy posible que esté ligeramente nublado o el dispositivo se encuentre bajo sombra.");
-  }
-  
-  if(luminosidad > 50 && luminosidad < 90){
-
-    Serial.println("Por tanto el disposotivo se encuentra expuesto a una luz ambiente estándar.");
-  }
-  
-  if(luminosidad >= 90 ){
-
-    Serial.println("Por tanto el dispositivo se encuentra expuesto a mucha luz.");
-  }
-
-  delay(5000);
-  }
 // Función espera 1s para leer del GPS
 static void smartDelay(unsigned long ms)
 {
@@ -151,6 +109,7 @@ static void smartDelay(unsigned long ms)
     }
   } while(millis() - start < ms);
 }
+
 // Función para encender/apagar mediante un pulso
 void switch_on_off()
 {
@@ -222,13 +181,7 @@ void setup() {
 
 void loop() {
   
-  //LUMINOSIDAD
-  
-  medicionLuminosidad();
-  delay(2000);
-  
-  //GPS
-  
+  //GPS *********************************************************************
   char gpsDate[10]; 
   char gpsTime[10];
 
@@ -259,29 +212,26 @@ void loop() {
   }
   smartDelay(1000);
   
-  //HUMEDAD
-  
+  //HUMEDAD *********************************************************************
   Serial.print("Humedad: ");
-  
   // Llamamos a la función calcularHumedad y nos calculará la humedad que recibe el sensor
   Serial.println(sensores.calcularHumedad(adcHumedad, AirValue, WaterValue)); // entra el adc 0, valor en aire y valor en agua
   
-  //SALINIDAD
-  
+  //SALINIDAD *********************************************************************
   Serial.print("Salinidad: ");
-
   // Llamamos a la función calcularSalinidad y nos calculará la salinidad que recibe el sensor
   Serial.println(sensores.calcularSalinidad(adcSalinidad, SinSalValue, SalValue, salinity_power_pin)); // entra el adc 1, valor sin sal, valor con sal y el pin 5
 
-  //TEMPERATURA
-  
+  //TEMPERATURA *********************************************************************
   Serial.print("Temperatura: ");
-
   // Llamamos a la función calcularTemperatura y nos calculará la temperatura que recibe el sensor
   Serial.println(sensores.calcularTemperatura(adcTemperatura)); // entra el adc 2
 
-  //DEEP SLEEP
-    
+  //LUMINOSIDAD *********************************************************************
+  Serial.print("Luminosidad: ");
+  sensores.calcularLuminosidad();
+
+  //DEEP SLEEP *********************************************************************
   Serial.print("-----------DORMIR----------");
   ESP.deepSleep(sleepTimeS * 1000000); // Ponemos la placa en reposo para que no gaste batería.
   
